8563662bd27babc7c84b6de8c2b00f048e636405
Vitor Oliveira <vtsoliveira2001@gmail.com>
2022-07-01 16:20:21 -0300

    subindo projeto server

---
+++ server/src/test/java/br/com/vitorcommerce/servercommerce/ServerCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitorcommerce.servercommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
